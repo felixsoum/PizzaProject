4720d287ca796d2217c3e2b08d08859e79e030be
Unknown <felix@felixsoum.com>
2019-01-13 22:55:35 -0500

    Add bacon

--- PizzaApp/PizzaApp/Program.cs
+++ PizzaApp/PizzaApp/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PizzaApp
+﻿namespace PizzaApp
 {
     class Program
     {
@@ -12,6 +6,7 @@ namespace PizzaApp
         {
             new Pepperoni();
             new Cheese();
+            new Bacon();
         }
     }
 }
